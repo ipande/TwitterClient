c48981c226afd13d2f1a54a2c1ffc29226e458dd
Ishan Pande <ishanpande@gmail.com>
2016-02-18 09:31:04 -0800

    Added text watcher to count chars

--- MySimpleTweets/app/src/main/java/com/codepath/apps/iTweetClient/fragments/TweetFragment.java
+++ MySimpleTweets/app/src/main/java/com/codepath/apps/iTweetClient/fragments/TweetFragment.java
@@ -4,11 +4,15 @@ package com.codepath.apps.iTweetClient.fragments;
 import android.support.v4.app.DialogFragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.codepath.apps.iTweetClient.R;
 
@@ -20,7 +24,10 @@ import butterknife.ButterKnife;
  */
 public class TweetFragment extends DialogFragment {
     @Bind(R.id.etTweet) EditText mTweetText;
-
+    @Bind(R.id.tvChars) TextView tvCharsLeft;
+    @Bind(R.id.btComposeTweet) Button btTweet;
+    final static int TWEET_LENGTH = 140;
+    int textColor;
 
 
     public TweetFragment() {
@@ -61,5 +68,35 @@ public class TweetFragment extends DialogFragment {
         getDialog().getWindow().setSoftInputMode(
                 WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
 
+        textColor = tvCharsLeft.getCurrentTextColor();
+        mTweetText.addTextChangedListener(textWatcher);
+
     }
+
+    TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            int charsRemaining = TWEET_LENGTH - s.length();
+            tvCharsLeft.setText(Integer.toString(charsRemaining));
+
+            if (charsRemaining >= 0 && charsRemaining < TWEET_LENGTH) {
+                btTweet.setEnabled(true);
+//                btnTweet.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                tvCharsLeft.setTextColor(getResources().getColor(android.R.color.holo_blue_dark));
+            } else {
+                btTweet.setEnabled(false);
+//                btnTweet.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                if (charsRemaining < 0)
+                    tvCharsLeft.setTextColor(getResources().getColor(android.R.color.holo_red_light));
+            }
+        }
+    };
 }
